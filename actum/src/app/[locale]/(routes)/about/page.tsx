84e0f067920ad521dbf51b34b228f593b63b538b
My <idla0003@stud.kea.dk>
2026-05-05 15:35:10 +0200

    feat: add About page and global styles with Tailwind CSS integration

---
+++ actum/src/app/[locale]/(routes)/about/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function AboutPage() {
+  return (
+    <main className="p-16">
+      <h1 className="text-2xl mb-4 font-ocr">Om Actum</h1>
+      <p className="text-base leading-relaxed font-sans">Actum Design og Tryk er et trykkeri på Vesterbro i København. Vi specialiserer os i print, der kræver tid, faglig viden og håndværk.</p>
+    </main>
+  );
+}
--- actum/src/app/[locale]/layout.tsx
+++ actum/src/app/[locale]/layout.tsx
@@ -2,11 +2,19 @@ import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import "./globals.css";
+import { Public_Sans } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Actum Design og Tryk",
   description: "Actum Design og Tryk",
 };
+
+const publicSans = Public_Sans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  display: "swap",
+  weight: ["200", "300", "400", "500", "600", "700"],
+});
 
 export default async function LocaleLayout({
   children,
@@ -20,7 +28,10 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
-      <body>
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/hga1vzf.css"/>
+      </head>
+      <body className={`${publicSans.variable} antialiased`}>
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
