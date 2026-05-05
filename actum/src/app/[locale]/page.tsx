87b5ab9822f2df393d62eab01bbe89bf4150cd43
Linnea <miebaekgaard@icloud.com>
2026-05-05 16:15:11 +0200

    feat: implement full bleed section and update layout for localization support

--- actum/src/app/[locale]/page.tsx
+++ actum/src/app/[locale]/page.tsx
@@ -1,16 +1,18 @@
 // import Image from "next/image";
 import { supabase } from "@/lib/supabaseClient";
+import FullBleedTest from "./fullbleed_test";
 
 export default async function Home() {
   // Hent data fra din nye tabel
-  const { data, error } = await supabase.from("personel").select("*");
+  // const { data, error } = await supabase.from("personel").select("*");
 
-  if (error) return <div>Fejl: {error.message}</div>;
+  // if (error) return <div>Fejl: {error.message}</div>;
 
   return (
-    <main className="p-20">
-      <p>{locale}</p>
-      <h1 className="text-2xl font-bold mb-4">Mit personale:</h1>
+    <main className="grid grid-cols-subgrid col-[full-start/full-end]">
+      <FullBleedTest />
+      {/* <p>{locale}</p> */}
+      {/* <h1 className="text-2xl font-bold mb-4">Mit personale:</h1>
       <ul className="list-disc pl-5">
         {data?.map((person, index) => (
           <li key={index} className="text-lg">
@@ -18,8 +20,8 @@ export default async function Home() {
           </li>
         ))}
       </ul>
-      {data?.length === 0 && <p>Ingen navne fundet.</p>}
-      <TestBanner locale={locale} />
+      {data?.length === 0 && <p>Ingen navne fundet.</p>} */}
+      {/* <TestBanner locale={locale} /> */}
     </main>
   );
 }
